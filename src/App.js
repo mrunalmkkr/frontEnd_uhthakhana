14feeb291b9c0e52c4f885e0dcb4e5ba5687ecd2
mrunal <mrunal@coalescence.tech>
2026-02-28 22:49:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import  Navbar from "./components/navBar";
-// import Footer from "./components/footer";
-// import  SideNav  from "./components/sideNav";
-// import Home from "./pages/home";
-// import Photos from "./pages/photos";
-// import Videos from "./pages/video";
-// import Philosophy from "./pages/philosophy";
-// import Dhamadeshana from "./pages/dhamadeshana";
-// import Login from "./pages/login";
-// import Register from "./pages/register";
-// import { Container } from "react-bootstrap";
-// import React, { useState } from "react";
-// import { LanguageProvider } from "./components/languageContext";
-
-
-// function App() { 
-  
-//   const [collapsed, setCollapsed] = useState(false);
-
-
-
-//   return (
-//     <>
-//       <LanguageProvider>
-//       <Navbar />
-//       <div style={{ display: "flex" }}>
-//         <SideNav onClick={setCollapsed}/>
-//         <div style={{ padding: "20px", width: "100%",alignItems:"center" }}>
-//         <Container className="p-5 m-5">
-        
-          
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/photos" element={<Photos />} />
-//             <Route path="/videos" element={<Videos />} />
-//             <Route path="/ambedkar-philosophy" element={<Philosophy />} />
-//             <Route path="/dhamadeshana" element={<Dhamadeshana />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/register" element={<Register />} />
-//           </Routes>
-         
-//           </Container>
-//         </div>
-//       </div>
-//       <Footer />
-//       </LanguageProvider>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navBar";
 import Footer from "./components/footer";
@@ -102,7 +46,7 @@ function App() {
                       
                         <Dhamadeshana />
                     
-                    }
+                      }
                   />
                   {/* PROTECTED ROUTES */}
                   <Route
